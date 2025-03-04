4d4dacbd21dcbf4238701cf2b88beba187608f0f
Kundan Kumar <kundankumar08950@gmail.com>
2025-03-05 00:47:26 +0530

    1780. Check if Number is a Sum of Powers of Three with all 3 approach

--- leetcode/Medium/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ leetcode/Medium/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -2,19 +2,47 @@
 using namespace std;
 class Solution{
   public: 
-  bool checkPowerOfThree(int n){
-    int p=0;
-    while(pow(3,p)<=n)p++;
-    while(n>0){
-      if(n>=pow(3,p)){
-        n=n-pow(3,p);
+  //approach 1
+  // bool checkPowerOfThree(int n){
+  //   int p=0;
+  //   while(pow(3,p)<=n)p++;
+  //   while(n>0){
+  //     if(n>=pow(3,p)){
+  //       n=n-pow(3,p);
+  //     }
+  //     if(n>=pow(3,p)){
+  //       return false;
+  //     }
+  //     p--;
+  //   }
+  //   return true;
+  // }
+  
+  //approach 2
+  // bool checkPowerOfThree(int n){
+  //   while(n>0){
+  //     if(n%3==2){
+  //       return false;
+  //     }
+  //     n/=3;
+  //   }
+  //   return true;
+  // }
+
+  //approach 3
+  bool solve(int n,int p){
+      if(n==0){
+        return true;
       }
-      if(n>=pow(3,p)){
+      if(pow(3,p)>n){
         return false;
       }
-      p--;
-    }
-    return true;
+      bool power_le_lo=solve(n-pow(3,p),p+1);
+      bool power_nahi_lo=solve(n,p+1);
+      return  power_le_lo||power_nahi_lo;
+  }
+  bool checkPowerOfThree(int n){
+      return solve(n,0);
   }
 };
 int main(){
